b5d34b497284f5351f49fab7e9d8a25f5c11f56e
Lingzhe Zhao <lzzhao@tongji.edu.cn>
2020-05-12 00:57:23 +0800

    init

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fiorella/devel/turtlebot3/catkin_ws/src
-# Build directory: /home/fiorella/devel/turtlebot3/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(simple_navigation_goals)
